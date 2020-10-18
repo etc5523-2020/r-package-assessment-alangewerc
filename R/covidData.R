a4b10396de830bbe058993ac32d158056e203053
Alan Gewerc <agew0001@student.monash.edu>
2020-10-18 15:19:12 +1100

    Create data folder

---
+++ R/covidData.R
@@ -0,0 +1,18 @@
+#' Our World in Data's Key Crop Yields
+#'
+#' A dataset containing crop yields for countries over time. Yields
+#' are measured in tonnes per hectare for seven crops.
+#'
+#' @format a tibble with 143825 observations and 5 variables:
+#'
+#' - **xxxx**: full country name
+#' - **xxxx**: three letter country code
+#' - **xxxx**: year from 1961 to 2018
+#' - **xxxx**: one of bananas, barley, beans, cassava, cocoa, maize, peas, potatoes, rice, soybeans, wheat
+#' - **xxxx**: the total yield of the crop, measured in tonnes per hectare
+#'
+#'
+#'@import tibble
+#'
+#'@source [Our World in Data](https://ourworldindata.org/crop-yields)  and [Tidy Tuesday](https://github.com/rfordatascience/tidytuesday/blob/master/data/2020/2020-09-01/readme.md)
+"covidData"
---
+++ data-raw/covidData.R
@@ -0,0 +1,13 @@
+## code to prepare `covidData` dataset goes here
+
+
+covidData <- read.csv('data-raw/covidData.csv')
+covidData$date <-  as.Date(covidData$date, format = "%m/%d/%Y")
+
+covidData <- melt(data = covidData, id.vars = c("Country", "date"), measure.vars = c("total_cases", "new_cases", "total_deaths", "new_deaths",
+                                                                                  "total_cases_per_million", "new_cases_per_million",
+                                                                                  "total_deaths_per_million", "new_deaths_per_million"))
+
+
+usethis::use_data(covidData, overwrite = TRUE)
+us
